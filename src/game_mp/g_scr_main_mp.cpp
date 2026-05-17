e0d93e600fad1d2c25bb769f0990179b6f276e48
Simasce <siamsce@gmail.com>
2026-05-17 19:17:44 +0300

    Added missing radian conversion to GScr_sin and GScr_cos

--- src/game_mp/g_scr_main_mp.cpp
+++ src/game_mp/g_scr_main_mp.cpp
@@ -6749,7 +6749,7 @@ void GScr_sin()
     Scr_AddFloat(*(float *)&value, v2);
 #endif
 
-    float value = Scr_GetFloat(0, SCRIPTINSTANCE_SERVER);
+    float value = Scr_GetFloat(0, SCRIPTINSTANCE_SERVER) * 0.017453292;
     value = sin(value);
     Scr_AddFloat(value, SCRIPTINSTANCE_SERVER);
 }
@@ -6769,7 +6769,7 @@ void GScr_cos()
     Scr_AddFloat(*(float *)&value, v2);
 #endif
 
-    float value = Scr_GetFloat(0, SCRIPTINSTANCE_SERVER);
+    float value = Scr_GetFloat(0, SCRIPTINSTANCE_SERVER) * 0.017453292;
     value = cos(value);
     Scr_AddFloat(value, SCRIPTINSTANCE_SERVER);
 }
